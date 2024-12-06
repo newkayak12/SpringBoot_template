e48b1371cb46ddc65b6d44e139329a7339b96d6b
newkayak12 <newkayak12@gmail.com>
2024-12-06 23:30:11 +0900

    swagger

--- persistence-layer/src/main/java/com/base/config/JpaConfig.java
+++ persistence-layer/src/main/java/com/base/config/JpaConfig.java
@@ -1,10 +1,11 @@
 package com.base.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-
 }
